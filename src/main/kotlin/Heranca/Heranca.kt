7d844254b40946a24eaad486d63d4efc4d34cb39
FranciellyDiasM <frandiasma@gmail.com>
2024-04-17 16:02:12 -0300

    liberar classe e metodo

--- src/main/kotlin/Heranca/Heranca.kt
+++ src/main/kotlin/Heranca/Heranca.kt
@@ -1,26 +1,33 @@
 package Heranca
 
-  class Animal{
-    fun dormir(){
+    // open para liberar essa classe p ser heranca
+  open class Animal{
+        // open para liberar o metodo
+    open fun dormir(){
         println("Dormir")
     }
+        fun correr(){
+            println("correr")
+        }
 
   }
 class Cao : Animal(){
+    // override -> para sobreescrever um metodo / precisa por open no metodo para liberar para ser sobreescrito
+    override fun dormir(){
+        println("Dormir como um cão.")
 
-
+    }
 }
-
 
 class Passaro: Animal(){
 
 }
 
-
 fun main (args: Array<String>){
 
     val cao = Cao()
     cao.dormir()
+    cao.correr()
 
     val passaro = Passaro()
     passaro.dormir()
